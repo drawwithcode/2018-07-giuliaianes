3beeef1f1126852f1d22ea224241ea2242d5278d
Giulia <giulia.ianes@gmail.com>
2018-11-02 01:02:05 +0100

    assignment

--- sketch.js
+++ sketch.js
@@ -1,11 +1,60 @@
-function preload(){
-  // put preload code here
+var micio;
+var barrel;
+var mic;
+var vol;
+var sfondo;
+
+function preload() {
+
 }
 
-function setup() {
-  // put setup code here
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    micio = loadImage('./assets/urlo.png');
+    sfondo = loadImage('./assets/sfondo.jpg');
+
+    barrel = new obstacle();
+    cat = new player();
+
+    mic = new p5.AudioIn();
+    mic.start();
+
 }
 
-function draw() {
-  // put drawing code here
+function draw(){
+  //background('pink');
+  image(sfondo, 0, 0);
+
+  barrel.display();
+  cat.display();
+
+  vol = mic.getLevel();
+  text(vol, 100, 200);
+}
+
+function player() {
+  this.display = function(){
+  console.log(vol);
+
+  var currentHeight=height/2;
+  if(vol>0.02) {
+  currentHeight-=vol*2500;
+  }
+image(micio, 200, currentHeight);
+
+
+  }
+}
+
+function obstacle() {
+  this.move = function() {
+  }
+  this.display = function() {
+    fill('blue');
+    rect(-frameCount+width, 500, 100, 100);
+  }
+}
+
+function volume() {
+
 }
